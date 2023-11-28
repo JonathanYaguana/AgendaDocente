3bff9af56797ed1fe2294d2a7f75d88b63b1c710
JonathanYaguana <jonathansacrox93@gmail.com>
2023-11-28 16:32:07 -0500

    Primer commit

---
+++ src/main/java/com/jmyc/AgendaDocente/AgendaDocenteApplication.java
@@ -0,0 +1,13 @@
+package com.jmyc.AgendaDocente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaDocenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaDocenteApplication.class, args);
+	}
+
+}
