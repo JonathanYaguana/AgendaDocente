428b444104fa5f875f63122ddc7cc7cf8b0bccb5
JonathanYaguana <jonathansacrox93@gmail.com>
2023-11-28 15:42:26 -0500

    Proyecto Agenda Docente

---
+++ src/test/java/com/jmyc/AgendaDocente/AgendaDocenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmyc.AgendaDocente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaDocenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
